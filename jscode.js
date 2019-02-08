5d3d8c32a27ae6eb0f8ef3ff332a68316aedbf33
bexkysx <xu.song@centre.edu>
2019-02-08 00:54:04 -0500

    try

--- jscode.js
+++ jscode.js
@@ -3,10 +3,22 @@ var change=function()
   var t=document.getElementById("head1")
   t.innerText="well done!"
 
-  var part1 = document.getElementById("part1");
-    part1.removeAttribute("disabled")
-
+  
 var abc=document.createElement("part1")
+
+
+}
+var great=function()
+{
+  var c=document.createElement('sroty')
+  sroty.innerHTML=
+  <form >
+  <input type="radio" name="story1" value="1">
+  <input type="radio" name="story1" value="2">
+  <input type="radio" name="story1" value="3">
+  </form>
+
+
 
 
 }
