a13f516b9ed3981756f16d5cf28efafd63c4ce1e
bexkysx <xu.song@centre.edu>
2019-02-08 00:42:03 -0500

    onclick

--- jscode.js
+++ jscode.js
@@ -3,7 +3,8 @@ var change=function()
   var t=document.getElementById("head1")
   t.innerText="well done!"
 
-
+  var part1 = document.getElementById("part1");
+    part1.removeAttribute("disabled")
 
 var abc=document.createElement("part1")
 
