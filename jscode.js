f5e70cd9074e74d72a6180acc69aff82a3e2a6a6
bexkysx <xu.song@centre.edu>
2019-02-08 01:06:41 -0500

    .

--- jscode.js
+++ jscode.js
@@ -4,25 +4,11 @@ var change=function()
   t.innerText="well done!"
 
 
+
 var abc=document.createElement("part1")
 
 
 }
-var great=function()
-{
-  var c=document.createElement('sroty')
-  sroty.innerHTML=
-  <form >
-  <input type="radio" name="story1" value="1">
-  <input type="radio" name="story1" value="2">
-  <input type="radio" name="story1" value="3">
-  </form>
-
-
-
-
-}
-
 var great=function()
 {
   var c=document.createElement('sroty')
