e9fbc66aba9486c063d366c9514fea2c3670e0aa
bexkysx <xu.song@centre.edu>
2019-02-19 20:15:53 -0500

    step 3

--- jscode.js
+++ jscode.js
@@ -1,24 +1,13 @@
-var change=function()
-{
-  var t=document.getElementById("head1")
-  t.innerText="well done!"
 
 
+var change=function(){
 
-}
-
-
-var great=function()
-{
-  var c=document.createElement('sroty')
-  sroty.innerHTML=
-  <form >
-  <input type="radio" name="story1" value="1">
-  <input type="radio" name="story1" value="2">
-  <input type="radio" name="story1" value="3">
-  </form>
-
-
+var a=document.getElementById("dog")
+a.innerText="Big dog"
+var b=document.getElementById("cat")
+b.innerText="Big cat"
+var c=document.getElementById("dragon")
+c.innerText="Big dragon"
 
 
 }
