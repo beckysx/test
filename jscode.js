804c453981a37ccccf9f9aa766ba6f7062272eb6
bexkysx <xu.song@centre.edu>
2019-03-05 17:49:54 -0500

    sdf

--- jscode.js
+++ jscode.js
@@ -1,12 +1,27 @@
-var change = function() {
-
-  var a = document.getElementsByTagName('li')
-  let b = Array.from(a)
-  b.forEach(function(d,index) {
-    b[index]=b[index].outerText;
-    a[index].innerHTML="big "+b[index]
-
-
-  })
-
+var dataPc=d3.csv("color.csv");
+var drawChartc = function(data){
+  var svg=d3.select(".csv")
+            .attr("width",width)
+            .attr("height",height);
+  svg.selectAll("rect")
+     .data(data)
+     .enter()
+     .append("rect")
+     .attr("x",function(d,i){
+       return i*(barwidth+barpadding)
+     })
+     .attr("y",function(d){
+       return height- d.num*20
+     })
+     .attr("width",barwidth)
+     .attr("height",function(d){
+       return d.num*20
+     })
+     .attr("fill",function(d){
+       return d.color
+     })
 }
+dataPc.then(function(data){
+  console.log(data),
+  drawChartc(data)
+})
